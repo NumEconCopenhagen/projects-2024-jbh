7fb35fe0989aa33dc6492c18a0d17d18d93ebbf1
Jørgen Høst <pjz633@alumni.ku.dk>
2024-04-09 16:35:21 +0200

    map + density plot + group_bys + to-do

--- dataproject/0_datapreprocess.py
+++ dataproject/0_datapreprocess.py
@@ -1,5 +1,6 @@
 import polars as pl
 import geopandas as gpd
+import pandas as pd
 
 # This takes the raw ARCOS dataset from Washington Post and converts to .parquet format to save on space
 lf = (pl.scan_csv('data/raw/arcos_all_washpost.tsv', separator='\t', dtypes={'TRANSACTION_DATE':pl.String, 'REPORTER_BUS_ACT':pl.Categorical, 'REPORTER_ZIP':pl.String, 'BUYER_BUS_ACT':pl.Categorical, 'BUYER_STATE':pl.Categorical, 'TRANSACTION_CODE':pl.Categorical, 'BUYER_ZIP':pl.String, 'DRUG_CODE':pl.Categorical, 'NDC_NO':pl.String, 'DRUG_NAME':pl.Categorical, 'TRANSACTION_ID':pl.String, 'Measure':pl.Categorical, 'Product_Name': pl.Categorical, 'Ingredient_Name': pl.Categorical}, ignore_errors=True)
@@ -25,13 +26,22 @@ gdf_counties.columns = [name.lower() for name in gdf_counties.columns]
 gdf_counties = gdf_counties[['statefp', 'countyfp', 'geoid', 'name', 'namelsad', 'geometry']]
 gdf_counties = gdf_counties[gdf_counties['statefp'].isin(states_fips)]
 gdf_counties = gdf_counties[gdf_counties['geoid'].isin(appa_count_list)]
-gdf_counties.to_parquet('data/us_appa_counties.pq')
+gdf_counties.reset_index(drop=True).to_parquet('data/us_appa_counties.pq')
 
 # Do the same, but filter for states
 gdf_states = gpd.read_file('data/raw/cb_2018_us_state_500k.shp')
 gdf_states.columns = [name.lower() for name in gdf_states.columns]
 gdf_states = gdf_states[gdf_states['statefp'].isin(states_fips)]
-gdf_states.to_parquet('data/us_appa_states.pq')
+gdf_states.reset_index(drop=True).to_parquet('data/us_appa_states.pq')
+
+# Filter for those pharmacies in the Central Appalachians
+df_pharm = pd.read_csv('data/pharmacies_latlon.csv')
+gdf_pharm = gpd.GeoDataFrame(
+    df_pharm, geometry=gpd.points_from_xy(df_pharm.lon, df_pharm.lat), crs=gdf_counties.crs
+)
+
+gdf_pharm=gdf_pharm.sjoin(gdf_counties[['geometry', 'geoid']], predicate='within').reset_index(drop=True)
+gdf_pharm.to_parquet('data/pharmacies_latlon.pq')
 
 # Data on pharmacies (https://github.com/wpinvestigative/arcos-api/blob/master/data/pharmacies_latlon14.csv)
 df_pharm = (pl.read_csv('data/raw/pharmacies_latlon14.csv')
@@ -39,9 +49,6 @@ df_pharm = (pl.read_csv('data/raw/pharmacies_latlon14.csv')
       .filter(pl.col("buyer_state").is_in(states))
 )
 list_of_buyers_in_appa = df_pharm.select(pl.col("buyer_dea_no")).to_series().to_list()
-
-df_pharm.write_csv('data/pharmacies_latlon.csv')
-
 
 # Reduce dimensionality of ARCOS dataset
 ## Filter for Central Appalachian states
@@ -56,3 +63,9 @@ lf = (pl.scan_parquet('data/raw/arcos_all_washpost.pq')
       .filter(pl.col("buyer_dea_no").is_in(list_of_buyers_in_appa))
 )
 lf.sink_parquet('data/arcos_appa.pq')
+
+# Which type of pharmacy --> add it to pharmacy data
+df_pharm_type = lf.select(pl.col("buyer_dea_no", 'buyer_bus_act')).unique().collect()
+
+df_pharm=df_pharm.join(df_pharm_type, left_on='buyer_dea_no', right_on='buyer_dea_no')
+df_pharm.write_csv('data/pharmacies_latlon.csv')
