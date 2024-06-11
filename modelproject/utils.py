dbdcb9b2b6571b7c76342b03cdf20a9a8c689254
Jørgen Høst <pjz633@alumni.ku.dk>
2024-06-11 14:32:59 +0200

    modelproj: refactor docs and comments

--- modelproject/utils.py
+++ modelproject/utils.py
@@ -5,6 +5,33 @@ import copy
 class ConSavModel:
     def __init__(self, r: float = 0.05, beta: float = 0.95, rho: float | int = 5, gamma: float | int = None, delta: float | int = None, kappa: float | int = None, sigma_low: float | int = None, sigma_high: float | int = None, p: float = 0.5):
 
+        '''
+        Initialize exchange consumption-saving model class.
+        Change model parameters as needed to analyze solutions.
+
+        Parameters:
+        -----
+
+            r: float | int 
+                Exogenous interest rate.
+            beta: float | int
+                Preference parameter for agent.
+            rho: float | int
+                Coefficient of risk aversion.
+            gamma: float | int 
+                Strength of bequest motive.
+            delta: float | int
+                "Risk" parameter.
+            kappa: float | int
+                Bequest parameter.
+            sigma_low: float | int
+                Under stochastic risk, lower boundary for risk value.
+            sigma_high: float | int
+                Under stochastic risk, upper boundary for risk value. 
+            p: float | int
+                Probability of ending in "low" income case.
+        '''
+
         self.rho = rho
         self.r = r
         self.beta = beta
@@ -16,16 +43,29 @@ class ConSavModel:
         self.p = p  
 
     def copy(self):
+        '''
+        Creates a copy of the current ConSavModel with same parameters.
+
+        Returns:
+        --------
+            ConSavModel: 
+                New instance of ConSavModel.
+        '''
         return copy.deepcopy(self)
 
     def solve_cons_crra(self, m1):
         '''
-        DESCRIPTION
+        Solve for the optimal consumption in period 1 and 2 given intial endowments. Assumes a constant relative risk aversion utility function.
 
         Parameters:
-            - SOMETHING
+        ------
+            m1: float
+                Intial wealth in period 1.
+
         Returns:
-            - SOMETHING ELSE
+        --------
+            tuple: 
+                A tuple containing the optimal consumption in period 1 and 2.
         '''
         
         # Objective func
@@ -42,16 +82,39 @@ class ConSavModel:
 
         return c1_star, c2_star
 
-    def utility_crra(self, c1):
-        util = (c1**(1-self.rho))/(1-self.rho)
+    def utility_crra(self, c):
+        '''
+        Constant relative risk aversion utility function.
+
+        Parameters:
+        ------
+            c: float
+                Consumption.
+
+        Returns:
+        --------
+            float: 
+                A numeric utility value.
+        '''
+        util = (c**(1-self.rho))/(1-self.rho)
         return util
 
-    def v2_func(self, c2, m2):
-        cons = (c2**(1-self.rho))/(1-self.rho)
-        beq = self.gamma*(m2-c2+self.kappa)**(1-self.rho)/(1-self.rho)
-        return cons+beq
-
     def v1_func(self, c1, m1, v2_interp_func):
+        '''
+        A (utility) value function in period 1. Assumes CRRA utility with bequest component. Includes expected utility in period 2.
+
+        Parameters:
+        ------
+            c1: float | int
+                Consumption in period 1.
+            m1: float | int
+                Wealth/endowment in period 1.
+
+        Returns:
+        --------
+            float | int: 
+                Value of utility in period 1 and expected utility in period 2.
+        '''
 
         y2_low = 1-self.delta
         y2_high = 1+self.delta
@@ -65,9 +128,24 @@ class ConSavModel:
         # total expected value in v2
         total = self.p * exp_v2_high + (1-self.p) * exp_v2_low
 
-        return self.utility_crra(c1, self.rho) + self.beta*total
+        return self.utility_crra(c1) + self.beta*total
     
     def v1_func_no_risk(self, c1, m1, v2_interp_func):
+        '''
+        A (utility) value function in period 1. Assumes CRRA utility with bequest component and no risk. Includes expected utility in period 2.
+
+        Parameters:
+        ------
+            c1: float | int
+                Consumption in period 1.
+            m1: float | int
+                Wealth/endowment in period 1.
+
+        Returns:
+        --------
+            float | int: 
+                Value of utility in period 1 and expected utility in period 2.
+        '''
 
         y2=1
         m2 = (1+self.r)*(m1-c1)+y2
@@ -78,7 +156,21 @@ class ConSavModel:
 
 
     def v1_func_stoch(self, c1, m1, sigma, v2_interp_func):
+        '''
+        A (utility) value function in period 1. Assumes CRRA utility with bequest component under stochastic income risk. Includes expected utility in period 2.
 
+        Parameters:
+        ------
+            c1: float | int
+                Consumption in period 1.
+            m1: float | int
+                Wealth/endowment in period 1.
+
+        Returns:
+        --------
+            float | int: 
+                Value of utility in period 1 and expected utility in period 2.
+        '''
         m2_low = (1+self.r)*(m1-c1)*(1-sigma)
         m2_high = (1+self.r)*(m1-c1)*(1+sigma)
 
@@ -91,12 +183,63 @@ class ConSavModel:
         return self.utility_crra(c1) + self.beta*total
 
     def interp(self, m_grid, v_grid):
+        '''
+        A (linear) interpolate function to derive expected utility one period ahead of time. 
+
+        Parameters:
+        ------
+            m_grid: np.ndarray
+                Grid points from where we interpolate.
+            v_grid: float | int
+                Values of function being interpolated at each grid point.
+
+        Returns:
+        --------
+            interpolator: 
+                An interpolator object to interpolate at points within or outside grid.
+        '''
         func = interpolate.RegularGridInterpolator((m_grid,), v_grid,
                                                         bounds_error=False,fill_value=None)
         return func
+    
+    def v2_func(self, c2, m2):
+        '''
+        A (utility) value function in period 2. Assumes CRRA utility with bequest component.
 
+        Parameters:
+        ------
+            c2: float | int
+                Consumption in period 2.
+            m2: float
+                Wealth in period 2.
+
+        Returns:
+        --------
+            float | int: 
+                Numeric value of utility in period 2.
+        '''
+
+        cons = (c2**(1-self.rho))/(1-self.rho)
+        beq = self.gamma*(m2-c2+self.kappa)**(1-self.rho)/(1-self.rho)
+        return cons+beq
+    
     def solve_period_1(self, v2_interp_func, v1):
-        
+        '''
+        Function that solves an agent's maximization problem in period 1.
+
+        Parameters:
+        ------
+            v2_interp_func: interpolator object
+                A function that interpolates values for period 2.
+            v1: objective function
+                The objective function of the agent in period 1.
+
+        Returns:
+        --------
+            tuple: 
+                A tuple of np.ndarrays with endowments (m1), value functions (v1) and consumption in period 1 (c1).
+        '''
+
         m1_grid = np.linspace(1e-8,4,100)
         v1_grid = np.empty(100)
         c1_grid = np.empty(100)
@@ -114,17 +257,24 @@ class ConSavModel:
             v1_grid[i] = -result.fun
             c1_grid[i] = result.x
         
-        return m1_grid,v1_grid,c1_grid
+        return m1_grid, v1_grid, c1_grid
 
     def solve_period_2(self):
-        
+        '''
+        Function that solves an agent's maximization problem in period 2. Takes only previously defined (exogenous) parameters.
+
+        Returns:
+        --------
+            tuple: 
+                A tuple of np.ndarrays with endowments (m2), value functions (v2) and consumption in period 2 (c2).
+        '''        
         # Defining grids
         m2_grid = np.linspace(1e-8,5,500)
         v2_grid = np.empty(500)
         c2_grid = np.empty(500)
 
         # b. solve for each m2 in grid
-        for i,m2 in enumerate(m2_grid):
+        for i, m2 in enumerate(m2_grid):
             
             # Defining obj func
             obj = lambda c2: -self.v2_func(c2, m2)
@@ -139,7 +289,22 @@ class ConSavModel:
         return m2_grid, v2_grid, c2_grid
 
     def solve_period_1_no_risk(self, v2_interp_func, v1):
-        
+        '''
+        Function that solves an agent's maximization problem in period 1 under no income risk.
+
+        Parameters:
+        ------
+            v2_interp_func: interpolator object
+                A function that interpolates values for period 2.
+            v1: objective function
+                The objective function of the agent in period 1.
+
+        Returns:
+        --------
+            tuple: 
+                A tuple of np.ndarrays with endowments (m1), value functions (v1) and consumption in period (c1).
+        '''
+
         m1_grid = np.linspace(1e-8,4,300)
         v1_grid = np.empty(300)
         c1_grid = np.empty(300)
@@ -158,48 +323,23 @@ class ConSavModel:
             c1_grid[i] = result.x
         
         return m1_grid,v1_grid,c1_grid
-
-    def solvez_no_risk(self, v1):
-        
-        # Solving for period 2
-        m2_grid, v2_grid, c2_grid = self.solve_period_2()
-
-        # Interpolator
-        v2_interp_func = self.interp(m2_grid, v2_grid)
-
-        # Solving for period 1
-        m1_grid, v1_grid, c1_grid = self.solve_period_1_no_risk(v2_interp_func, v1)
-        
-        return m1_grid, c1_grid, m2_grid, c2_grid
-
-    def solvez(self, v1):
-        
-        # Solving for period 2
-        m2_grid, v2_grid, c2_grid = self.solve_period_2()
-
-        # Interpolator
-        v2_interp_func = self.interp(m2_grid, v2_grid)
-
-        # Solving for period 1
-        m1_grid, v1_grid, c1_grid = self.solve_period_1(v2_interp_func, v1)
-        
-        return m1_grid, c1_grid, m2_grid, c2_grid
-
-    def solvez_stoch(self, v1):
-        
-        # Solving for period 2
-        m2_grid, v2_grid, c2_grid = self.solve_period_2()
-
-        # Interpolator
-        v2_interp_func = self.interp(m2_grid, v2_grid)
-
-        # Solving for period 1
-        m1_grid, v1_grid, c1_grid = self.solve_period_1_stoch(v2_interp_func, v1)
-        
-        return m1_grid, c1_grid, m2_grid, c2_grid
-
+    
     def solve_period_1_stoch(self, v2_interp_func, v1):
-        
+        '''
+        Function that solves an agent's maximization problem in period 1 under stochastic income risk. Risk parameters are defined when instantiating the model.
+
+        Parameters:
+        ------
+            v2_interp_func: interpolator object
+                A function that interpolates values for period 2.
+            v1: objective function
+                The objective function of the agent in period 1.
+
+        Returns:
+        --------
+            tuple: 
+                A tuple of np.ndarrays with endowments (m1), value functions (v1) and consumption in period (c1).
+        '''
         m1_grid = np.linspace(1e-8,5,300)
         v1_grid = np.empty(300)
         c1_grid = np.empty(300)
@@ -220,3 +360,78 @@ class ConSavModel:
             c1_grid[i] = result.x
         
         return m1_grid, v1_grid, c1_grid
+    
+    def solvez_no_risk(self, v1):
+        '''
+        Compound that solves the agent's maximization problem under no income risk in its entirety.
+
+        Parameters:
+        ------
+            v1: objective function
+                The objective function of the agent in period 1.
+
+        Returns:
+        --------
+            tuple: 
+                A tuple of np.ndarrays with endowments (m1/m2) and consumption in period 1 and period 2 (c1/c2).
+        '''
+        # Solving for period 2
+        m2_grid, v2_grid, c2_grid = self.solve_period_2()
+
+        # Interpolator
+        v2_interp_func = self.interp(m2_grid, v2_grid)
+
+        # Solving for period 1
+        m1_grid, v1_grid, c1_grid = self.solve_period_1_no_risk(v2_interp_func, v1)
+        
+        return m1_grid, c1_grid, m2_grid, c2_grid
+
+    def solvez(self, v1):
+        '''
+        Compound that solves the agent's maximization problem in its entirety.
+
+        Parameters:
+        ------
+            v1: objective function
+                The objective function of the agent in period 1.
+
+        Returns:
+        --------
+            tuple: 
+                A tuple of np.ndarrays with endowments (m1/m2) and consumption in period 1 and period 2 (c1/c2).
+        '''        
+        # Solving for period 2
+        m2_grid, v2_grid, c2_grid = self.solve_period_2()
+
+        # Interpolator
+        v2_interp_func = self.interp(m2_grid, v2_grid)
+
+        # Solving for period 1
+        m1_grid, v1_grid, c1_grid = self.solve_period_1(v2_interp_func, v1)
+        
+        return m1_grid, c1_grid, m2_grid, c2_grid
+
+    def solvez_stoch(self, v1):
+        '''
+        Compound that solves the agent's maximization problem under stochastic income risk in its entirety.
+
+        Parameters:
+        ------
+            v1: objective function
+                The objective function of the agent in period 1.
+
+        Returns:
+        --------
+            tuple: 
+                A tuple of np.ndarrays with endowments (m1/m2) and consumption in period 1 and period 2 (c1/c2).
+        '''        
+        # Solving for period 2
+        m2_grid, v2_grid, c2_grid = self.solve_period_2()
+
+        # Interpolator
+        v2_interp_func = self.interp(m2_grid, v2_grid)
+
+        # Solving for period 1
+        m1_grid, v1_grid, c1_grid = self.solve_period_1_stoch(v2_interp_func, v1)
+        
+        return m1_grid, c1_grid, m2_grid, c2_grid
